fb72dfda256effba983e10f019510b3bc8686bfb
Abdo <abdo.sw.eng@gmail.com>
2024-08-18 21:43:17 +0300

    Edit in questions info texts

--- lib/categories_data.dart
+++ lib/categories_data.dart
@@ -1,42 +1,44 @@
 Map<String, dynamic> categoriesData = {
   "1": [
     "Punctuality: Started on time exactly up to 5 minutes late.",
-    "Dress code:No accessories and piercings, clean classic shoes, clean not torn classic attire, well trimmed beard and keep hairstyles groomed.",
+    "Dress code:No accessories and piercings, clean classic shoes, clean not torn classic attire, well trimmed beard and keep hairstyles groomed."
   ],
   "2": [
     "The Rep can get complete data about his product, competitors, potential customers, consumption, market trends with absolute figures.",
   ],
   "3": [
-    "Review customer thinking preference,treatment paradigm and customer medical degree.",
+    "Comprehensive pre-call preparation before each visit, including a review of the customer’s thinking preferences, treatment paradigm, and customer medical degree.",
   ],
   "4": [
     "Self Intro: Name and Company Name.",
-    "Patient profile: Painting a word picture via describing a very detailed patient profile including signs, symptoms and using body language.",
+    "Patient Profile: Continuously describe a detailed patient profile, including signs, symptoms, and body language, at each visit",
   ],
   "5": [
-    "The Rep is confident asking different types of high value questions.",
+    "Uncovering Needs: The Rep confidently asks at least 1 type of high-value questions in every visit to identify needs",
     "Active Listening: No interruptions, confirm/clarify.",
   ],
   "6": [
-    "Link product feature(s) with customer need: Tell benefits about the product based on needs expressed from the customer with all categories and types of the customers.",
+    "Link product feature(s) with customer need: The Rep consistently links product features to customer needs in every visit, tailoring benefits to all customer types and categories.",
   ],
   "7": [
     "APACT: A=Acknowledge, P=Probing, A=Answer ,C=Confirm and T=Transition.",
   ],
   "8": [
-    "Ask for specific commitment: The types of commitment you ask for should be based on where the customer is on the Brand Bonding Ladder.",
+    "Summarize: The Rep consistently summarizes the agreed benefits in every visit.",
+    "Ask for specific commitment:The Rep consistently asks for a specific commitment in every visit, tailored to the customer's position on the Brand Bonding Ladder"
   ],
   "9": [
-    "Helps customers follow the discussion and know what to expect next.",
+    "Bridging to P2: Bridging to second product",
+    "The Rep consistently helps customers follow the discussion and know what to expect next."
   ],
   "10": [
-    "Creating an effective reminder involves summarizing key details concisely.",
+    "Creating an effective reminder involves summarizing key details concisely and doing so consistently every visit.",
   ],
   "11": [
-    "The rep records outcomes and self-assesses, identifying strengths and areas for improvement.",
+    "The rep records outcomes and self-assesses after each visit, using data to identify strengths and areas for improvement.",
   ],
   "12": [
-    "The Rep knows his area well and able to plan for good routing plan to use his time effectively.",
+    "The rep continuously manages time and territory by planning effective routes, having a Plan B for unexpected situations, and scheduling visits to ensure each does not exceed 30 minutes.",
   ],
 
   //
